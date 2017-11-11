5825aefe60a46a88bb876de11051f5343b5ab967
David Crotty <david.crotty.work@gmail.com>
2017-11-11 21:58:38 +0000

    Get and delete

--- todos-api/app/controllers/todos_controller.rb
+++ todos-api/app/controllers/todos_controller.rb
@@ -4,8 +4,22 @@ class TodosController < ApplicationController
     render json: { it: 'works' }
   end
 
+  # POST todos
   def create
     Todo.create!(title: 'test', created_by: 'david')
     render status: :ok
+  end
+
+  # GET todo/:id
+  def show
+    # Route automatically maps the id
+    todo = Todo.find(params[:id])
+    render(json: todo, status: :ok)
+  end
+
+  # DELETE todo/:id
+  def destroy
+    Todo.find(params[:id]).destroy
+    render(status: :ok)
   end
 end
