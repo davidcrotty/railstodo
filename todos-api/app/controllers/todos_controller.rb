fe07529496a660db26dfa7d988544904be66ca59
David Crotty <david.crotty.work@gmail.com>
2017-11-11 21:43:09 +0000

    post

--- todos-api/app/controllers/todos_controller.rb
+++ todos-api/app/controllers/todos_controller.rb
@@ -3,4 +3,9 @@ class TodosController < ApplicationController
   def index
     render json: { it: 'works' }
   end
+
+  def create
+    Todo.create!(title: 'test', created_by: 'david')
+    render status: :ok
+  end
 end
